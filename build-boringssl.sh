61b3f942af57e1aba084281857f170bd22b84156
Mathias Schreiner <m.schreiner@dracoon.com>
2020-02-21 14:55:16 +0100

    Fixed build script

--- build-boringssl.sh
+++ build-boringssl.sh
@@ -21,21 +21,21 @@ build_framework()
   cd ..
 }
 
-#if [ -d boringssl ]; then
-#rm -rf boringssl
-#fi
+if [ -d boringssl ]; then
+rm -rf boringssl
+fi
 
-#git clone https://boringssl.googlesource.com/boringssl
+git clone https://boringssl.googlesource.com/boringssl
 cd boringssl
 for ARCH in arm64 armv7
 do
 build_framework $ARCH "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS9.3.sdk"
 done
-for ARCH in x86_64 i386
+for ARCH in x86_64
 do
 build_framework $ARCH "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator9.3.sdk"
 done
-lipo -create -output "arm64/openssl.framework/openssl" "arm64/openssl.framework/openssl" "armv7/openssl.framework/openssl" "x86_64/openssl.framework/openssl" "i386/openssl.framework/openssl"
+lipo -create -output "arm64/openssl.framework/openssl" "arm64/openssl.framework/openssl" "armv7/openssl.framework/openssl" "x86_64/openssl.framework/openssl"
 if [ -d ../output ]; then
 rm -rf ../output
 fi
