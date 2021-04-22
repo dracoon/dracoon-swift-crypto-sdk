565083620d05bf1d3dff5e262e957fcfcc711ac5
Mathias Schreiner <m.schreiner@dracoon.com>
2021-04-22 11:33:44 +0200

    Deleted build example folder

--- build_examples/Pods/Target Support Files/DRACOON-Crypto-SDK/DRACOON-Crypto-SDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DRACOON_Crypto_SDK : NSObject
-@end
-@implementation PodsDummy_DRACOON_Crypto_SDK
-@end
--- build_examples/Pods/Target Support Files/Pods-CryptoSDKCocoaPodsExample/Pods-CryptoSDKCocoaPodsExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CryptoSDKCocoaPodsExample : NSObject
-@end
-@implementation PodsDummy_Pods_CryptoSDKCocoaPodsExample
-@end
