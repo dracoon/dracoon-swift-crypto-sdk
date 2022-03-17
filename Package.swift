ea50d8224d60123f8a50502fdc00406e968cc88e
Mathias Schreiner <m.schreiner@dracoon.com>
2022-03-17 14:53:18 +0100

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -19,10 +19,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "boringSSL", path: "OpenSSL/openssl.xcframework"),
+        .binaryTarget(name: "openssl", path: "OpenSSL/openssl.xcframework"),
         .target(
             name: "sdk-crypto-objc",
-            dependencies: ["boringSSL"],
+            dependencies: ["openssl"],
             path: "crypto-sdk/crypto",
             publicHeadersPath: "include",
             cSettings: [
